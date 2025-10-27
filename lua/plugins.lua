acaae8ed5b144ede88136ffc6a9fa29cf3e5f13a
liveneutron <liveneutron@proton.me>
2025-10-27 10:45:35 +0530

    add ascii kitty to dashboard

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,7 @@
- local ensure_packer = function()
+-- ensuring packer is installed
+local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     vim.cmd [[packadd packer.nvim]]
@@ -11,7 +12,57 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function()
-        -- packer can manage itself
-        use 'wbthomason/packer.nvim'
+return require('packer').startup(function(use)
+  -- Packer
+  use 'wbthomason/packer.nvim'
+
+  ------------------------------------------------------------------
+  -- DASHBOARD
+  ------------------------------------------------------------------
+    use {
+        'goolord/alpha-nvim',
+        config = function()
+        local alpha = require('alpha')
+        local dashboard = require('alpha.themes.dashboard')
+
+        -- Your ASCII Art
+        local ascii = {
+        ' ,_     _   ',
+        ' |\\\\_,-~/   ',
+        ' / _  _ |    ,--. ',
+        '(  @  @ )   / ,-\' ',
+        ' \\  _T_/-._( (   ',
+        ' /         `. \\  ',
+        '|         _  \\ | ',
+        ' \\ \\ ,  /      | ',
+        '  || |-\\_\\__   / ',
+        ' ((_/`(____,-\'  ',
+        }
+
+        dashboard.section.header.val = ascii
+
+        -- Auto-calculate vertical padding to center the art
+        local height = #ascii
+        local win_height = vim.fn.winheight(0)
+        local padding = math.floor((win_height - height) / 2)
+        if padding < 0 then padding = 0 end
+
+        dashboard.opts.layout = {
+        { type = "padding", val = padding },
+        dashboard.section.header,
+        }
+
+        -- Header and Footer
+        dashboard.section.buttons.val = {}
+        dashboard.section.footer.val = {}
+
+        -- Optional: highlight the ASCII art
+        dashboard.section.header.opts.hl = "String"  -- or "Include", "Title", etc.
+
+        -- Disable unnecessary autocmds
+        dashboard.opts.opts.noautocmd = true
+
+        alpha.setup(dashboard.opts)
+    end,
+    }
 end)
