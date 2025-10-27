45415b471b44d2472de2dbfce0227660dfcb8a40
liveneutron <liveneutron@proton.me>
2025-10-27 16:03:03 +0530

    add date, time and file size function

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -1,5 +1,6 @@
 -- STATUS LINE
 vim.o.laststatus = 3
+-- Which mode
 local modes = {
     ['n']    = 'NORMAL',
     ['no']   = 'N·OPERATOR PENDING',
@@ -29,14 +30,71 @@ end
 
 _G.mode = mode
 
+-- function to get current time
+function _G.get_statusline_time()
+  return os.date('%I:%M:%S %p')
+end
+
+-- function to get current date
+function _G.get_statusline_date()
+  return os.date('%d/%m/%y')
+end
+
+--function to get file size
+function _G.file_size()
+  local buf = vim.api.nvim_get_current_buf()
+  local path = vim.api.nvim_buf_get_name(buf)
+
+  -- Skip non‑file buffers (scratch, help, terminal, etc.)
+  if path == '' or vim.fn.filereadable(path) == 0 then
+    return ''
+  end
+
+  local ok, stat = pcall(vim.loop.fs_stat, path)
+  if not ok or not stat then
+    return ''
+  end
+
+  local size = stat.size               -- bytes (integer)
+  if size < 1024 then
+    return string.format('%d B', size)
+  elseif size < 1024 * 1024 then
+    return string.format('%.1f KB', size / 1024)
+  elseif size < 1024 * 1024 * 1024 then
+    return string.format('%.1f MB', size / (1024 * 1024))
+  else
+    return string.format('%.1f GB', size / (1024 * 1024 * 1024))
+  end
+end
+
 local statusline = {
     ' %#Search#%{v:lua.mode()}%#StatusLine# ',
     '%t ',
     '%r',
     '%=',
-    '[%{&filetype}] ',
+    '%#Search# %{%v:lua.get_statusline_date()%} %#Statusline# ',
+    '%#Search# %{%v:lua.get_statusline_time()%} %#Statusline#',
+    '%=',
+    '[%{&filetype}]',
+    '[%{%v:lua.file_size()%}] ',
     '%2p%% ',
     '%#Search# %04L:%04l:%02c %#StatusLine#'
 }
 
 vim.o.statusline = table.concat(statusline, '')
+
+-- Function to redraw statusline
+function redrawstatus()
+    vim.cmd("redrawstatus")
+end
+
+-- Timer to for redrawstatus function
+function start_timer()
+    vim.defer_fn(function()
+        redrawstatus()
+        start_timer()
+    end, 1000)
+end
+
+-- Start timer
+start_timer()
